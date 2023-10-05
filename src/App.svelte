44d72a4a1325419057a3d177fd85666e5d22cfaf
taxirov <saadtakhir1@gmail.com>
2023-10-05 23:06:05 +0500

    start project

---
+++ src/App.svelte
@@ -0,0 +1,130 @@
+<div
+    class="fixed top-6 left-20 flex justify-between w-[90%] px-8 py-4 mx-auto bg-gray-800 rounded-2xl"
+>
+    <div class="flex justify-start items-center gap-4">
+        <div class="w-8 h-8 rounded-2xl crop">
+            <img
+                class="w-[29px] h-[29px] rounded-2xl"
+                src="./90a3be0312ae0f2090f08b2fdb6d7828.png"
+                alt="icon"
+            />
+        </div>
+        <div class="text-white text-lg font-medium font-['Poppins']">
+            Saad Takhir
+        </div>
+    </div>
+    <div class="flex gap-4 items-center">
+        <a
+            href="#home"
+            class="text-gray-200 hover:text-white text-md font-medium hover:font-semibold"
+            >Home</a
+        >
+        <a
+            href="#skills"
+            class="text-gray-200 hover:text-white text-md font-medium">Skills</a
+        >
+        <a
+            href="#portfolio"
+            class="text-gray-200 hover:text-white text-md font-medium"
+            >Portfolio</a
+        >
+        <a
+            href="#blog"
+            class="text-gray-200 hover:text-white text-md font-medium">Blog</a
+        >
+    </div>
+    <div class="justify-center items-center gap-[17px] flex">
+        <div
+            class="px-[36.06px] py-[8.65px] bg-indigo-400 rounded-lg justify-center items-center gap-[7.21px] flex"
+        >
+            <div class="text-white text-lg font-semibold font-['Poppins']">
+                Contact Me
+            </div>
+        </div>
+    </div>
+</div>
+<div id="home" class="flex justify-between items-end h-screen w-full pb-[64px]">
+    <div class="flex flex-col items-start pb-[2rem] gap-3 w-1/2">
+        <h1 class="text-7xl font-bold text-gray-800">Assalomu aleykum</h1>
+        <p class="text-md font-medium text-gray-800 w-3/4">
+            Assalomu aleykum. Men ismim Saad Takhir. Men 22 yoshdaman. Men +1
+            yilik tajribaga ega backend dasturchiman. Frontend va UI/UX dizayn
+            sohalariga ham qiziqaman.
+        </p>
+        <div class="flex gap-3 items-center">
+            <a
+                href="mailto:saadtakhir1@gmail.com"
+                class="px-6 py-2 text-white font-semibold rounded-xl bg-indigo-600"
+                >Email<i class="bi bi-envelope-fill ml-3" /></a
+            >
+            <a
+                href="https:/t.me/saad_takhir"
+                class="px-6 py-2 text-white font-semibold rounded-xl bg-sky-600"
+                >Telegram<i class="bi bi-telegram ml-3" /></a
+            >
+            <a
+                href="https:/t.me/saad_takhir"
+                class="px-6 py-2 text-white font-semibold rounded-xl bg-green-600"
+                >Call<i class="bi bi-telephone-fill ml-3" /></a
+            >
+        </div>
+    </div>
+    <div class="w-1/2">
+        <img class="w-3/4" src="./Software developer.png" alt="" />
+    </div>
+</div>
+<div id="skills" class="flex flex-col gap-4 pt-[120px] h-screen text-gray-800">
+    <h1 class="text-4xl font-semibold">Skills</h1>
+    <div class="p-4 shadow-sm rounded-2xl w-[100%] bg-white">
+        <h2 class="text-xl">
+            <b>Programming language:</b> Javascript, Typescript
+        </h2>
+    </div>
+    <div class="p-4 border-2 rounded-2xl w-[100%]">
+        <h2 class="text-xl">
+            <b>Other:</b> Nodejs, Expressjs, Prisma, Firebase, JWT, OOP, ORM(prisma.io)
+        </h2>
+    </div>
+    <div class="p-4 border-2 rounded-2xl w-[100%]">
+        <h2 class="text-xl"><b>Database:</b> SQL, PostgreSQL, MangoDB</h2>
+    </div>
+    <div class="p-4 border-2 rounded-2xl w-[100%]">
+        <h2 class="text-xl">
+            <b>Frontend:</b> HTML, CSS, Tailwind CSS, Bootstrap, SvelteJS, Axios
+        </h2>
+    </div>
+    <div class="p-4 border-2 rounded-2xl w-[100%]">
+        <h2 class="text-xl">
+            <b>Design:</b> UI Design, UX Design, Database Design, SMD
+        </h2>
+    </div>
+</div>
+<div id="portfolio" class="flex flex-col gap-4 pt-[120px] h-screen">
+    <h1 class="text-4xl font-semibold text-gray-800">Portfolio</h1>
+    <div class="grid grid-cols-3 gap-4">
+        <a
+            href="https://github.com/taxirov"
+            target="_blank"
+            class="flex flex-col items-center justify-between shadow bg-gray-800 rounded-2xl text-white pb-8"
+        >
+            <i class="bi bi-github text-[14rem]" />
+            <p class="text-4xl font-medium">GitHub</p>
+        </a>
+        <a
+            href="https://figma.com/@taxirov"
+            target="_blank"
+            class="flex flex-col items-center justify-between shadow bg-white rounded-2xl text-gray-800 pt-[4rem] pb-8"
+        >
+            <img src="./Figma-logo.svg" alt="" class="h-[14rem]" />
+            <p class="text-4xl font-medium text-gray-800">Figma</p>
+        </a>
+        <a
+            href="https://t.me/saad_blog"
+            target="_blank"
+            class="flex flex-col items-center justify-between shadow bg-sky-500 rounded-2xl text-white pb-8"
+        >
+            <i class="bi bi-telegram text-[14rem]" />
+            <p class="text-4xl font-medium">Telegram</p>
+        </a>
+    </div>
+</div>
